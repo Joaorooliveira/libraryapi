d297e191fbaac58bd6369ba39f8aef0809f822d5
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-18 16:45:36 -0300

    feat: adiciona entidade livro

--- src/main/java/io/github/joaorooliveira/libraryapi/model/Livro.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/model/Livro.java
@@ -1,4 +1,44 @@
 package io.github.joaorooliveira.libraryapi.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "isbn", nullable = false, length = 20)
+    private String isbn;
+
+    @Column(name = "titulo", nullable = false, length = 150)
+    private String titulo;
+
+    @Column(name = "data_publicacao", nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", nullable = false, length = 30)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private Double preco;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
 }
