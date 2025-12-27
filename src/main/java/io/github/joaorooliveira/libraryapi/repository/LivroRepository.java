620be58f15abc9734708fd2b5e3d2647718f0013
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-27 00:13:57 -0300

    feat(livro-repository): adiciona metodo para buscar livros por autor especifico

--- src/main/java/io/github/joaorooliveira/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/repository/LivroRepository.java
@@ -1,9 +1,16 @@
 package io.github.joaorooliveira.libraryapi.repository;
 
+import io.github.joaorooliveira.libraryapi.model.Autor;
 import io.github.joaorooliveira.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
+
+
+    //Query method
+    //select * from livro where id_autor = id
+    List<Livro> findByAutor(Autor autor);
 }
