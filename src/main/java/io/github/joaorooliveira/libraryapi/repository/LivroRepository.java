b2eb803c01fa50d0cc0cbd356feb2f1ba3e642b5
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-30 14:38:25 -0300

    feat(livro-repository): adiciona metodo para deletar com query

--- src/main/java/io/github/joaorooliveira/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/repository/LivroRepository.java
@@ -4,8 +4,10 @@ import io.github.joaorooliveira.libraryapi.model.Autor;
 import io.github.joaorooliveira.libraryapi.model.GeneroLivro;
 import io.github.joaorooliveira.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -72,4 +74,13 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
     @Query(" select l from Livro l where l.genero = ?1 order by ?2 ")
     List<Livro> findByGeneroPositionalParameters(GeneroLivro genero, String paramOrdenacao);
 
+    @Transactional
+    @Modifying
+    @Query(" delete from Livro where genero = ?1")
+    void deleteByGenero(GeneroLivro genero);
+
+    @Transactional
+    @Modifying
+    @Query(" update Livro set dataPublicacao = ?1")
+    void updateDataPublicacao(LocalDate novaData);
 }
