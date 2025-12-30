0dd7e650d0d06376c69de3509b3665cb019f1e2c
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-30 13:14:54 -0300

    feat(livro-repository): adiciona metodo para buscar por titulo e preco

--- src/main/java/io/github/joaorooliveira/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/repository/LivroRepository.java
@@ -4,6 +4,7 @@ import io.github.joaorooliveira.libraryapi.model.Autor;
 import io.github.joaorooliveira.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,4 +14,15 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
     //Query method
     //select * from livro where id_autor = id
     List<Livro> findByAutor(Autor autor);
+
+
+    //select *from livro where titulo = 'titulo'
+    List<Livro> findByTitulo(String titulo);
+
+    List<Livro> findByIsbn(String isbn);
+
+    List<Livro> findByTituloAndPreco(String titulo, BigDecimal preco);
+
+    // select *from livro where titulo = 'titulo' or isbn = 'isbn'
+    List<Livro> findByTituloOrIsbn(String titulo, String isbn);
 }
