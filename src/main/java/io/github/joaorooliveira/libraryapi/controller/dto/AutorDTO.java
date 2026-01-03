018f4de0c3e791f8892d3f98594025ec4007b101
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-03 17:33:54 -0300

    feat(autor-dto): adiciona atributo de ID

--- src/main/java/io/github/joaorooliveira/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/controller/dto/AutorDTO.java
@@ -3,8 +3,10 @@ package io.github.joaorooliveira.libraryapi.controller.dto;
 import io.github.joaorooliveira.libraryapi.model.Autor;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public record AutorDTO(
+        UUID id,
         String nome,
         LocalDate dataNascimento,
         String nacionalidade) {
