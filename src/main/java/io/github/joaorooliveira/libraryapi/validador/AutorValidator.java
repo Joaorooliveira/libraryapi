9863a5ea56cf5a6dc52a6fb66063c7d4fdaf84a1
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-03 20:38:30 -0300

    feat: adiciona classe de validacao para autor

--- src/main/java/io/github/joaorooliveira/libraryapi/validador/AutorValidator.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/validador/AutorValidator.java
@@ -1,4 +1,35 @@
 package io.github.joaorooliveira.libraryapi.validador;
 
+import io.github.joaorooliveira.libraryapi.exceptions.RegistroDuplicadoException;
+import io.github.joaorooliveira.libraryapi.model.Autor;
+import io.github.joaorooliveira.libraryapi.repository.AutorRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class AutorValidator {
+
+    private AutorRepository repository;
+
+    public AutorValidator(AutorRepository repository) {
+        this.repository = repository;
+    }
+
+    public void validar(Autor autor) {
+        if (existeAutorCadastrado(autor)) {
+            throw new RegistroDuplicadoException("Autor ja cadastrado!");
+        }
+    }
+
+    private boolean existeAutorCadastrado(Autor autor) {
+        Optional<Autor> autorEncontrado = repository.findByNomeAndDataNascimentoAndNacionalidade(
+                autor.getNome(), autor.getDataNascimento(), autor.getNacionalidade()
+        );
+        if (autor.getId() == null) {
+            return autorEncontrado.isPresent();
+        }
+
+        return !autor.getId().equals(autorEncontrado.get().getId()) && autorEncontrado.isPresent();
+    }
 }
