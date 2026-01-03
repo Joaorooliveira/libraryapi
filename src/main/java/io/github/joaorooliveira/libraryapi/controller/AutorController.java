748deef1d6577dfb5b3a5e0aafa1b2ad5e232af7
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-03 18:49:11 -0300

    feat(autor-controller): adiciona endpoint para buscar autor com filtros

--- src/main/java/io/github/joaorooliveira/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/controller/AutorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -71,6 +72,21 @@ public class AutorController {
         }
         service.deletar(autorOptional.get());
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade) {
+        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
+                .stream()
+                .map(autor -> new AutorDTO(autor.getId(),
+                        autor.getNome(),
+                        autor.getDataNascimento(),
+                        autor.getNacionalidade()))
+                .toList();
+        return ResponseEntity.ok(lista);
     }
 
 }
