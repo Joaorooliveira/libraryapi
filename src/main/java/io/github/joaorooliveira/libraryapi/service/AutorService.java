d8438f2e893924f96049ab8b26c311e6c4e352f6
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-03 17:34:41 -0300

    feat(autor-service): adiciona metodo para buscar autor por id

--- src/main/java/io/github/joaorooliveira/libraryapi/service/AutorService.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/service/AutorService.java
@@ -4,6 +4,9 @@ import io.github.joaorooliveira.libraryapi.model.Autor;
 import io.github.joaorooliveira.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class AutorService {
 
@@ -15,5 +18,21 @@ public class AutorService {
 
     public Autor salvar(Autor autor) {
         return autorRepository.save(autor);
+    }
+
+//    public AutorDTO buscarAutor(UUID id) {
+//        var autor = autorRepository.findById(id).orElseThrow(()
+//                -> new EntityNotFoundException("Autor não encontrado com id: " + id));
+//
+//        return new AutorDTO(
+//                autor.getId(),
+//                autor.getNome(),
+//                autor.getDataNascimento(),
+//                autor.getNacionalidade()
+//        );
+
+    public Optional<Autor> obterPorId(UUID id) {
+        return autorRepository.findById(id);
+
     }
 }
