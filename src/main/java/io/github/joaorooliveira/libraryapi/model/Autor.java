6161bd7ace81f46db5821fc3a18ec8faf0e82afe
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-03 16:53:11 -0300

    feat(autor): adiciona anotacao de auditoria para a entidade

--- src/main/java/io/github/joaorooliveira/libraryapi/model/Autor.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/model/Autor.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString(exclude = "livros")
+@EntityListeners(AuditingEntityListener.class)
 public class Autor {
 
     @Id
