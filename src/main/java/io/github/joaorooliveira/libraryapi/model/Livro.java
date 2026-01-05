3f21f8b71a34b15f6ef80cb5aeaf8490cbde0d14
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-05 12:33:49 -0300

    feat(livro): adiciona atributos e anotacoes de auditoria

--- src/main/java/io/github/joaorooliveira/libraryapi/model/Livro.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/model/Livro.java
@@ -2,9 +2,13 @@ package io.github.joaorooliveira.libraryapi.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -14,6 +18,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(exclude = "autor")
+@EntityListeners(AuditingEntityListener.class)
 public class Livro {
 
     @Id
@@ -40,4 +45,17 @@ public class Livro {
     @ManyToOne(fetch = FetchType.LAZY)//(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_autor")
     private Autor autor;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
+
+
 }
