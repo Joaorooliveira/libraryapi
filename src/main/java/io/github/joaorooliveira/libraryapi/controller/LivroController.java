2549f6f16bb739b61b042ca45c77ad2676f967ff
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-05 12:34:04 -0300

    feat: adiciona livro controller

--- src/main/java/io/github/joaorooliveira/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/controller/LivroController.java
@@ -1,4 +1,15 @@
 package io.github.joaorooliveira.libraryapi.controller;
 
+import io.github.joaorooliveira.libraryapi.service.LivroService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("livros")
+@RequiredArgsConstructor
 public class LivroController {
+
+    private final LivroService livroService;
+    
 }
