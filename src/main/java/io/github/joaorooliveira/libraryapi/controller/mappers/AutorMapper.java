b4b880f8d42d5e9a3743b38a46bceae54eab95af
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-05 14:18:32 -0300

    feat: adiciona interface mapper struct

--- src/main/java/io/github/joaorooliveira/libraryapi/controller/mappers/AutorMapper.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/controller/mappers/AutorMapper.java
@@ -1,4 +1,16 @@
 package io.github.joaorooliveira.libraryapi.controller.mappers;
 
+import io.github.joaorooliveira.libraryapi.controller.dto.AutorDTO;
+import io.github.joaorooliveira.libraryapi.model.Autor;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface AutorMapper {
+
+    @Mapping(source = "nome", target = "nome")
+// quando o campo do dto e diferente do campo da entidade
+    Autor toEntity(AutorDTO dto);
+
+    AutorDTO toDTO(Autor autor);
 }
