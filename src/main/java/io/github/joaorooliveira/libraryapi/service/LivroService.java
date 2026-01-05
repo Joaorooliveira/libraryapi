46b694fda8b1a2b8d9220619a0da4ea041d1ca0b
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-05 12:34:17 -0300

    feat: adiciona livro service

--- src/main/java/io/github/joaorooliveira/libraryapi/service/LivroService.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/service/LivroService.java
@@ -1,4 +1,14 @@
 package io.github.joaorooliveira.libraryapi.service;
 
+import io.github.joaorooliveira.libraryapi.repository.LivroRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class LivroService {
+
+    private final LivroRepository livroRepository;
+
+    
 }
