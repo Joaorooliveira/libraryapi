cb9f3a5f0143964495eb3041a10e99d0998cd412
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-07 20:36:10 -0300

    feat(livro-controller): implementa interface generic controller

--- src/main/java/io/github/joaorooliveira/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/controller/LivroController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("livros")
 @RequiredArgsConstructor
-public class LivroController {
+public class LivroController implements GenericController {
 
     private final LivroService service;
     private final LivroMapper mapper;
@@ -31,7 +31,8 @@ public class LivroController {
         try {
             Livro livro = mapper.toEntity(dto);
             service.salvar(livro);
-            return ResponseEntity.ok(livro);
+            var url = gerarHeaderLocation(livro.getId());
+            return ResponseEntity.created(url).build();
         } catch (RegistroDuplicadoException e) {
             var erroDTO = ErroResposta.conflito(e.getMessage());
             return ResponseEntity.status(HttpStatus.CONFLICT).body(erroDTO);
