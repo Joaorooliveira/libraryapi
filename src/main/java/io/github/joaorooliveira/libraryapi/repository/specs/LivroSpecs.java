a0c02de4cedc8c23bc2f184e90bd889bcbee808f
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-12 15:51:05 -0300

    feat: adiciona classe livro-specs

--- src/main/java/io/github/joaorooliveira/libraryapi/repository/specs/LivroSpecs.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/repository/specs/LivroSpecs.java
@@ -1,4 +1,17 @@
 package io.github.joaorooliveira.libraryapi.repository.specs;
 
+import io.github.joaorooliveira.libraryapi.model.Livro;
+import org.springframework.data.jpa.domain.Specification;
+
 public class LivroSpecs {
+
+    public static Specification<Livro> isbnEqual(String isbn) {
+        return (root, query, cb) -> cb.equal(root.get("isbn"), isbn);
+    }
+
+    public static Specification<Livro> tituloLike(String titulo) {
+        //upper(livro.titulo) like (%:param%)
+        return (root, query, cb) ->
+                cb.like(cb.upper(root.get("titulo")), "%" + titulo.toUpperCase() + "%");
+    }
 }
