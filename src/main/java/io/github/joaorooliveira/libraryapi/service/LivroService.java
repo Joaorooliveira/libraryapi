ff030d752612c66722e732007431b4f8a36d6c0c
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-12 16:52:07 -0300

    feat(livro-service): adiciona criterio de busca por data de publicacao

--- src/main/java/io/github/joaorooliveira/libraryapi/service/LivroService.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/service/LivroService.java
@@ -1,12 +1,17 @@
 package io.github.joaorooliveira.libraryapi.service;
 
+import io.github.joaorooliveira.libraryapi.model.GeneroLivro;
 import io.github.joaorooliveira.libraryapi.model.Livro;
 import io.github.joaorooliveira.libraryapi.repository.LivroRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static io.github.joaorooliveira.libraryapi.repository.specs.LivroSpecs.*;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +30,44 @@ public class LivroService {
 
     public void deletar(Livro livro) {
         repository.delete(livro);
+    }
+
+    // isbn,titulo,nome autor, genero, ano de publicacao
+    public List<Livro> pesquisa(String isbn,
+                                String titulo,
+                                String nomeAutor,
+                                GeneroLivro genero,
+                                Integer anoPublicacao) {
+
+        // select * from livro where isbn = "isbn and nomeAutor =
+
+//        Specification<Livro> specs = Specification
+//                .where(LivroSpecs.isbnEqual(isbn))
+//                .and(LivroSpecs.generoEqual(genero))
+//                .and(LivroSpecs.tituloLike(titulo));
+
+        // select * from livro where 0=0
+        Specification<Livro> specs = Specification.where((root, query, cb) -> cb.conjunction());
+
+        if (isbn != null) {
+            // query = query +  and isbn = :isbn"
+            specs = specs.and(isbnEqual(isbn));
+        }
+
+        if (titulo != null) {
+            specs = specs.and(tituloLike(titulo));
+        }
+
+        if (genero != null) {
+            // query = query +  and isbn = :isbn"
+            specs = specs.and(generoEqual(genero));
+        }
+
+        if (anoPublicacao != null) {
+            specs = specs.and(anoPublicacaoEqual(anoPublicacao));
+        }
+
+        return repository.findAll(specs);
     }
 }
 
