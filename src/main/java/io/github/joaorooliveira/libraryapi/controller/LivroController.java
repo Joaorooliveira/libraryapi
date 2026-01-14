7b40223d4a32b949d5ffc127574f7f6279580825
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-13 21:42:21 -0300

    refactor(livro-controller): adiciona parametros de paginacao para endpoint de pesquisar livro

--- src/main/java/io/github/joaorooliveira/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/controller/LivroController.java
@@ -8,10 +8,10 @@ import io.github.joaorooliveira.libraryapi.model.Livro;
 import io.github.joaorooliveira.libraryapi.service.LivroService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -51,7 +51,7 @@ public class LivroController implements GenericController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ResultadoPesquisaLivroDTO>> pesquisa(
+    public ResponseEntity<Page<ResultadoPesquisaLivroDTO>> pesquisa(
             @RequestParam(value = "isbn", required = false)
             String isbn,
             @RequestParam(value = "titulo", required = false)
@@ -61,14 +61,16 @@ public class LivroController implements GenericController {
             @RequestParam(value = "genero", required = false)
             GeneroLivro genero,
             @RequestParam(value = "ano-publicacao", required = false)
-            Integer anoPublicacao
+            Integer anoPublicacao,
+            @RequestParam(value = "pagina", defaultValue = "0")
+            Integer pagina,
+            @RequestParam(value = "tamanho-pagina", defaultValue = "10")
+            Integer tamanhoPagina
     ) {
-        var resultado = service.pesquisa(isbn, titulo, nomeAutor, genero, anoPublicacao);
-        var lista = resultado.
-                stream()
-                .map(mapper::toDTO)
-                .toList();
-        return ResponseEntity.ok(lista);
+        Page<Livro> paginaResultado = service.pesquisa(isbn, titulo, nomeAutor, genero, anoPublicacao, pagina, tamanhoPagina);
+
+        Page<ResultadoPesquisaLivroDTO> resultado = paginaResultado.map(mapper::toDTO);
+        return ResponseEntity.ok(resultado);
     }
 
     @PutMapping({"{id}"})
