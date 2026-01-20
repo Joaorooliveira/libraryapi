c2e2e3f8c5230e3466fe56e2c2d14054be29000f
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-19 21:11:26 -0300

    feat: adiciona login controller

--- src/main/java/io/github/joaorooliveira/libraryapi/controller/LoginViewController.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/controller/LoginViewController.java
@@ -1,4 +1,13 @@
 package io.github.joaorooliveira.libraryapi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginViewController {
+
+    @GetMapping("/login")
+    public String paginaLogin() {
+        return "login";
+    }
 }
