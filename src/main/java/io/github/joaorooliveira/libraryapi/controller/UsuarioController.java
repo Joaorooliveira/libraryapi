77de4db38ebf68cc735153666f1024a32697a661
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-24 19:52:14 -0300

    feat: adiciona usuario controller

--- src/main/java/io/github/joaorooliveira/libraryapi/controller/UsuarioController.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/controller/UsuarioController.java
@@ -1,4 +1,26 @@
 package io.github.joaorooliveira.libraryapi.controller;
 
+import io.github.joaorooliveira.libraryapi.controller.dto.UsuarioDTO;
+import io.github.joaorooliveira.libraryapi.controller.mappers.UsuarioMapper;
+import io.github.joaorooliveira.libraryapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService service;
+    private final UsuarioMapper mapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // retorno fixo 201 Created(nao dinamico)
+    public void salvar(@RequestBody UsuarioDTO dto) {
+        var usuario = mapper.toEntity(dto);
+        service.salvar(usuario);
+    }
+
+
 }
