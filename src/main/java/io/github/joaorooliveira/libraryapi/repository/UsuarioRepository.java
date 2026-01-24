d42e7753d10a7001f58cd64f8d9af5dee23054e7
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-24 19:16:06 -0300

    feat: adiciona usuario repository

---
+++ src/main/java/io/github/joaorooliveira/libraryapi/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package io.github.joaorooliveira.libraryapi.repository;
+
+import io.github.joaorooliveira.libraryapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+}
