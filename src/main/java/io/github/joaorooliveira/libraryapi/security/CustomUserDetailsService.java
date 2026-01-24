b1d24daf87805a5a63fe2d78fc418d9875ad6e9d
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-01-24 20:27:07 -0300

    feat: adiciona classe para customizar o user datails service

--- src/main/java/io/github/joaorooliveira/libraryapi/security/CustomUserDetailsService.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/security/CustomUserDetailsService.java
@@ -1,4 +1,31 @@
 package io.github.joaorooliveira.libraryapi.security;
 
-public class CustomUserDetailsService {
+import io.github.joaorooliveira.libraryapi.model.Usuario;
+import io.github.joaorooliveira.libraryapi.service.UsuarioService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioService service;
+
+    public CustomUserDetailsService(UsuarioService service) {
+        this.service = service;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Usuario usuario = service.obterPorLogin(login);
+        if (usuario != null) {
+            throw new UsernameNotFoundException("Usuario nao encontrado");
+        }
+
+        return User.builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(usuario.getRoles().toArray(new String[usuario.getRoles().size()]))
+                .build();
+    }
 }
