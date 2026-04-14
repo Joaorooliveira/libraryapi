152c54850e3b18a8acdb04ac95b128386a4c7820
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-04-14 19:57:42 -0300

    feat(usuario-dto): adiciona validacao para email

--- src/main/java/io/github/joaorooliveira/libraryapi/controller/dto/UsuarioDTO.java
+++ src/main/java/io/github/joaorooliveira/libraryapi/controller/dto/UsuarioDTO.java
@@ -1,6 +1,8 @@
 package io.github.joaorooliveira.libraryapi.controller.dto;
 
+import jakarta.validation.constraints.Email;
+
 import java.util.List;
 
-public record UsuarioDTO(String login, String email, String senha, List<String> roles) {
+public record UsuarioDTO(String login, @Email String email, String senha, List<String> roles) {
 }
